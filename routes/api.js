535b3131736d5c1338cb350a4935ad6cddac6f17
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-17 14:23:53 +0000

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -9,26 +9,65 @@
 'use strict';
 
 var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
-const MONGODB_CONNECTION_STRING = process.env.DB;
+let mongodb = require('mongodb');
+let mongoose = require('mongoose');
+
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
 module.exports = function (app) {
+  
+  let uri = process.env.DB;
+  mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology:true});
+  let bookSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    comments: [String]
+  });
 
+  let Book = mongoose.model('Book', bookSchema);
+  
   app.route('/api/books')
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      let arrayOfBooks = [];
+      Book.find({}, (error, results) => {
+        if (!error && results) {
+          results.forEach((result) => {
+            let book = result.toJSON();
+            book['commentcount'] = book.comments.length;
+            arrayOfBooks.push(book);
+          });
+          return res.json(arrayOfBooks);
+        }
+      });
     })
     
     .post(function (req, res){
       var title = req.body.title;
       //response will contain new book object including atleast _id and title
+      if (!title) {
+        return res.json('missing title');
+      }
+
+      let newBook = new Book({
+        title: title,
+        comments: []
+      });
+
+      newBook.save((error, savedBook) => {
+        if (!error && savedBook) {
+          res.json(savedBook);
+        }
+      });
     })
     
     .delete(function(req, res){
       //if successful response will be 'complete delete successful'
+      Book.remove({}, (error, jsonStatus) => {
+        if (!error && jsonStatus) {
+          return res.json('complete delete successful');
+        }
+      });
     });
 
 
@@ -37,17 +76,41 @@ module.exports = function (app) {
     .get(function (req, res){
       var bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      Book.findById(bookid, (error, result) => {
+        if (!error && result) {
+          return res.json(result);
+        } else if (!result) {
+          return res.json('no book exists');
+        }
+      });
     })
     
     .post(function(req, res){
       var bookid = req.params.id;
       var comment = req.body.comment;
       //json res format same as .get
+      Book.findByIdAndUpdate(bookid, 
+      {$push: {comments:comment}},
+      {new: true},
+      (error, updatedBook) => {
+        if (!error && updatedBook) {
+          return res.json(updatedBook);
+        } else if (!updatedBook) {
+          return res.json('no book exists');
+        }
+      });
     })
     
     .delete(function(req, res){
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
+      Book.findByIdAndRemove(bookid, (error, deletedBook) => {
+        if (!error && deletedBook) {
+          return res.json('delete successful');
+        } else if (!deletedBook) {
+          return res.json('no book exists');
+        }
+      });
     });
   
 };
